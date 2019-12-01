aa4f143726228aa23a1a2e27a4a47e3145cfa26b
Michael Lafreniere <temike1@gmail.com>
2019-12-01 11:27:28 -0800

    Added a couple more modules (bad-words and express rate limit), started to style the site.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const assert = require('assert');
 const path = require('path');
 const mysql = require('mysql2');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 const {
+  NODE_ENV,
   HOST,
   PORT,
   SQL_SERVER,
@@ -15,6 +17,8 @@ const {
   SQL_PASSWORD
 } = process.env;
 
+// Verify we have all the data from the .env file we need:
+assert(NODE_ENV, 'NODE_ENV value not found in .env file.');
 assert(HOST, 'HOST value not found in .env file.');
 assert(PORT, 'PORT value not found in .env file.');
 assert(SQL_SERVER, 'SQL_SERVER value not found in .env file.');
@@ -22,11 +26,40 @@ assert(SQL_DATABASE, 'SQL_DATABASE value not found in .env file.');
 assert(SQL_USER, 'SQL_USER value not found in .env file.');
 assert(SQL_PASSWORD, 'SQL_PASSWORD value not found in .env file.');
 
+//
+// Setup the express server:
+//
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/public/views'));
 app.set('port', PORT);
+app.use(cors());
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // Request methods you wish to allow
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.json());
-//app.use(express.static(path.join(__dirname + '/public')));
+//app.use(express.urlencoded());
+// Check for production setup:
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //
 // Connect to MySQL DB:
@@ -37,7 +70,7 @@ const connection = mysql.createConnection({
   password: SQL_PASSWORD,
   database: SQL_DATABASE
 });
-
+// Check the connection:
 connection.connect(error => {
   if (error) throw error;
   console.log(`Connected to ${SQL_DATABASE}`);
@@ -48,6 +81,9 @@ connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
   console.log('The solution is: ', results[0].solution);
 });
 
+//
+// Handle the default route:
+//
 app.get('/', (req, res, next) => {
   res.render('index', {});
 });
