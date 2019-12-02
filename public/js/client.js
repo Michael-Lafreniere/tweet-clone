c87caced34f4206f4fde7094c4ffa9ee58fb2afd
Michael Lafreniere <temike1@gmail.com>
2019-12-02 12:26:24 -0800

    Quick backup of the code before I debug

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.tweeter-form');
+const loadingElement = document.querySelector('.loading');
 
+const API_URL = 'http://localhost:3000/post';
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const tweeter = formData.get('tweeter');
+
+  const chirp = { name, tweeter };
+
+  console.log(chirp);
+
+  form.style.display = 'none';
+  loadingElement.style.display = '';
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(chirp),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+});
--- server.js
+++ server.js
@@ -33,22 +33,22 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/public/views'));
 app.set('port', PORT);
 app.use(cors());
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
+// app.use((req, res, next) => {
+//   // Website you wish to allow to connect
+//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   // Request methods you wish to allow
+//   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-Requested-With,content-type'
+//   );
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Pass to next layer of middleware
+//   next();
+// });
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.json());
 //app.use(express.urlencoded());
@@ -76,16 +76,24 @@ connection.connect(error => {
   console.log(`Connected to ${SQL_DATABASE}`);
 });
 // Double check the connection:
-connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+// connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
 
 //
 // Handle the default route:
 //
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index', {});
+});
+
+// app.get('/chirps', (req, res) => {
+//   res.json({ message: 'chirp chirp!' });
+// });
+
+app.post('/post', (req, res) => {
+  console.log(req.body());
 });
 
 //
